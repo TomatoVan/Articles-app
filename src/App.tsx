00583f1f8e2cb0e19c5ffeec5f940b9ffc4ccbc7
Leon.kov <kovzel.leo@gmail.com>
2023-09-28 19:45:49 +0300

    feat: add styles, dark \ light themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Suspense} from 'react';
 import {Counter} from "./components/Counter";
-import './index.scss'
+import './styles/index.scss'
 import {Link, Route, Routes,} from "react-router-dom";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 
+export enum Theme {
+	LIGHT = 'normal',
+	DARK = 'dark'
+}
+
 const App = () => {
+
+
+	const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+
+	const toggleTheme = () => {
+		setTheme(theme => theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT)
+	}
+
 	return (
-	<div className='app'>
+	<div className={`app ${theme}`}>
+		<button onClick={toggleTheme}>TOGGLE</button>
 		<Link to={'/'}>Главная</Link>
 		<Link to={'/about'}>О сайте</Link>
 		<Suspense fallback={<div>Loading...</div>}>
