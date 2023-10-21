963f79643a762aae61c31172ec0cc3f3d4285b3a
Leon.kov <kovzel.leo@gmail.com>
2023-10-21 19:50:08 +0300

    feat: navlink \ navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss';
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
 
 interface NavbarProps {
 	className?: string;
@@ -12,8 +12,8 @@ export const Navbar = ({className}: NavbarProps) => {
 	return (
 	<div className={classNames(cls.Navbar, {}, [className] )}>
 		<div className={cls.links}>
-			<Link className={cls.mainLink} to={'/'}>Главная</Link>
-			<Link to={'/about'}>О сайте</Link>
+			<AppLink theme={AppLinkTheme.SECONDARY} className={cls.mainLink} to={'/'}>Главная</AppLink>
+			<AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>О сайте</AppLink>
 		</div>
 	</div>
 	);
