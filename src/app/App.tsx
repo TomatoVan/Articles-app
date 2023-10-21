5b792a0cf4f83209132c15402b7888c647ad51ef
Leon.kov <kovzel.leo@gmail.com>
2023-10-21 14:54:30 +0300

    feat: add main page routing config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import './styles/index.scss'
-import {Link, Route, Routes,} from "react-router-dom";
+import {Link,} from "react-router-dom";
 import {useTheme} from "app/providers";
 import {classNames} from "shared/lib/classNames/classNames";
-import {MainPage} from "pages/MainPage";
-import {AboutPage} from "pages/AboutPage";
+import {AppRouter} from "app/providers/router";
 
 
 const App = () => {
@@ -16,13 +15,7 @@ const App = () => {
 		<button onClick={toggleTheme}>TOGGLE</button>
 		<Link to={'/'}>Главная</Link>
 		<Link to={'/about'}>О сайте</Link>
-		<Suspense fallback={<div>Loading...</div>}>
-
-			<Routes>
-				<Route path={'/'} element={<MainPage/>}/>
-				<Route path={'/about'} element={<AboutPage/>}/>
-			</Routes>
-		</Suspense>
+		<AppRouter/>
 
 	</div>
 	);
---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,22 @@
+import React, {Suspense} from 'react';
+import {Route, Routes} from "react-router-dom";
+import {RouteConfig} from "shared/config/routeConfig/routeConfig";
+
+const AppRouter = () => {
+	return (
+	<Suspense fallback={<div>Loading...</div>}>
+
+		<Routes>
+			{Object.values(RouteConfig).map((element, path) => (
+				<Route
+					key={path}
+					path={path.toString()}
+					element={element}
+				/>
+			))}
+		</Routes>
+	</Suspense>
+	);
+};
+
+export default AppRouter;
