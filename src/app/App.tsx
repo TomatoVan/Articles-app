de0bc3c4cac7319fa44d4a6abc852f8679377526
Leon.kov <kovzel.leo@gmail.com>
2023-10-22 17:59:12 +0300

    feat: add svg, btns shared components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,14 +8,12 @@ import {Navbar} from "widgets/Navbar";
 
 const App = () => {
 
-	const {theme, toggleTheme} = useTheme();
+	const {theme} = useTheme();
 
 	return (
 	<div className={classNames('app', {}, [theme])}>
 
 		<Navbar/>
-		<button onClick={toggleTheme}>TOGGLE</button>
-
 		<AppRouter/>
 
 	</div>
--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -1,4 +1,9 @@
 import ThemeProvider from "./ThemeProvider/ui/ThemeProvider";
-import {useTheme} from "app/providers/ThemeProvider/lib/useTheme";
+import {useTheme} from "./ThemeProvider/lib/useTheme";
+import {Theme} from "./ThemeProvider/lib/ThemeContext";
 
-export {ThemeProvider, useTheme}
+export {
+	ThemeProvider,
+	useTheme,
+	Theme
+}
