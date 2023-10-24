861a039e1a90f35d3b5b4c8f3ebfb36b53920f66
Leon.kov <kovzel.leo@gmail.com>
2023-10-24 14:11:25 +0300

    feat: fix routes

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,29 +1,25 @@
-import React, {Suspense} from 'react';
-import {Route, Routes} from "react-router-dom";
-import {RouteConfig} from "shared/config/routeConfig/routeConfig";
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { RouteConfig } from "shared/config/routeConfig/routeConfig";
 
 const AppRouter = () => {
-	return (
-	<Suspense fallback={<div>Loading...</div>}>
-
-		<Routes>
-			{Object.values(RouteConfig).map((element, path) => (
-				<Route
-					key={path}
-					path={path.toString()}
-					element={(
-					<Suspense fallback={<div>Loading...</div>}>
-						<div className="page-wrapper">
-							{element}
-						</div>
-					</Suspense>
-					)}
-
-				/>
-			))}
-		</Routes>
-	</Suspense>
-	);
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        {Object.values(RouteConfig).map(({ element, path }) => (
+          <Route
+            key={path}
+            path={path}
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <div className="page-wrapper">{element}</div>
+              </Suspense>
+            }
+          />
+        ))}
+      </Routes>
+    </Suspense>
+  );
 };
 
 export default AppRouter;
