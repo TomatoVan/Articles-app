d7e03cae5081244c8104effd46da4e39b73682a2
Leon.kov <kovzel.leo@gmail.com>
2023-10-24 14:06:54 +0300

    feat: add i18n translations \ sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,41 +1,25 @@
-import React, {Suspense} from 'react';
-import './styles/index.scss'
-import {useTheme} from "app/providers";
-import {classNames} from "shared/lib/classNames/classNames";
-import {AppRouter} from "app/providers/router";
-import {Navbar} from "widgets/Navbar";
-import {useTranslation} from "react-i18next";
-
-
-const Component = () => {
-	const {t, i18n} = useTranslation();
-
-	const toggle = () => {
-		i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
-	}
-
-	return (
-	<div>
-		<button onClick={toggle}>{t('test')}</button>
-	</div>
-	)
-
-}
+import React, { Suspense } from "react";
+import "./styles/index.scss";
+import { useTheme } from "app/providers";
+import { classNames } from "shared/lib/classNames/classNames";
+import { AppRouter } from "app/providers/router";
+import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
 
 const App = () => {
+  const { theme } = useTheme();
 
-	const {theme} = useTheme();
-
-	return (
-	<div className={classNames('app', {}, [theme])}>
-		<Suspense fallback="">
-			<Component/>
-			<Navbar/>
-			<AppRouter/>
-		</Suspense>
-
-	</div>
-	);
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <Suspense fallback="">
+        <Navbar />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import {classNames} from "shared/lib/classNames/classNames";
-import cls from './Navbar.module.scss';
-import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
-import {ThemeSwitcher} from "shared/ui/ThemeSwitcher/ThemeSwitcher";
+import React from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./Navbar.module.scss";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
 
 interface NavbarProps {
-	className?: string;
+  className?: string;
 }
 
-
-export const Navbar = ({className}: NavbarProps) => {
-	return (
-	<div className={classNames(cls.Navbar, {}, [className] )}>
-		<ThemeSwitcher/>
-		<div className={cls.links}>
-			<AppLink theme={AppLinkTheme.SECONDARY} className={cls.mainLink} to={'/'}>Главная</AppLink>
-			<AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>О сайте</AppLink>
-		</div>
-	</div>
-	);
+export const Navbar = ({ className }: NavbarProps) => {
+  return (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+      <div className={cls.links}>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          className={cls.mainLink}
+          to={"/"}
+        >
+          Главная
+        </AppLink>
+        <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>
+          О сайте
+        </AppLink>
+      </div>
+    </div>
+  );
 };
