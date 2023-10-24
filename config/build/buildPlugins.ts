f4d79d8283df4350fe876f823964a0fa23ff92ad
Leon.kov <kovzel.leo@gmail.com>
2023-10-24 21:39:46 +0300

    feat: add babel, webpack settings

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,24 +1,29 @@
 import HTMLWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
-import {BuildOptions} from "./types/config";
+import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
-export function buildPlugins ({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
-	return [
-		//to create html files  to serve our bundles
-		new HTMLWebpackPlugin({
-			//to use html file in public as template
-			template: paths.html
-		}),
-		//build progression
-		new webpack.ProgressPlugin(),
-		new MiniCssExtractPlugin({
-			filename:'css/[name].[contenthash:8].css',
-			chunkFilename: 'css/[name].[contenthash:8].css'
-		}),
-		new webpack.DefinePlugin({
-			__IS_DEV__: JSON.stringify(isDev),
-		})
-
-	]
+export function buildPlugins({
+  paths,
+  isDev,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
+  return [
+    //to create html files  to serve our bundles
+    new HTMLWebpackPlugin({
+      //to use html file in public as template
+      template: paths.html,
+    }),
+    //build progression
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash:8].css",
+      chunkFilename: "css/[name].[contenthash:8].css",
+    }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+    new ReactRefreshPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ];
 }
