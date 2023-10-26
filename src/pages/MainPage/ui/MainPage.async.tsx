d9e6e11ba950a6172acfb12250fc1906173addfa
Leon.kov <kovzel.leo@gmail.com>
2023-10-26 18:57:26 +0300

    feat: add stylelint for styles linters and i18next linters

--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -1,3 +1,3 @@
-import {lazy} from "react";
-//нужен, чтобы чанки банлдов подгружались отдельно друг от друга
-export const MainPageAsync = lazy(() => import ('./MainPage'))
+import { lazy } from 'react';
+// нужен, чтобы чанки банлдов подгружались отдельно друг от друга
+export const MainPageAsync = lazy(() => import('./MainPage'));
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import {useTranslation} from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
+    const { t } = useTranslation();
 
-	const {t} = useTranslation();
-
-	return (
-	<div>
-		{t("main_page")}
-	</div>
-	);
+    return (
+        <div>
+            {t('main_page')}
+        </div>
+    );
 };
 
 export default MainPage;
