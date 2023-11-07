aac564a7084fd37738fccee99eca5ec8d24113c0
Leon.kov <kovzel.leo@gmail.com>
2023-11-07 14:19:20 +0300

    feat: add redux

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,6 +13,9 @@ function App() {
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
+                <button type="button" onClick={() => setIsOpen(true)}>
+                    toggle
+                </button>
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
