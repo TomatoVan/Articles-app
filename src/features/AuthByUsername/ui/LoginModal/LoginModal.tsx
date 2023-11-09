9864f07e745174bc58149f7ff2f5af10d329ee48
Leon.kov <kovzel.leo@gmail.com>
2023-11-09 19:30:38 +0300

    feat: husky changes

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,16 +1,14 @@
-import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from 'features/AuthByUsername/ui/LoginForm/LoginForm';
-import cls from './LoginModal.module.scss';
 
 interface LoginModalProps {
-	className?: string;
-	isOpen: boolean;
-	onClose: () => void;
+  className?: string;
+  isOpen: boolean;
+  onClose: () => void;
 }
 
 export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => (
-    <Modal lazy onClose={onClose} isOpen={isOpen} className={classNames(cls.LoginModal, {}, [className])}>
+    <Modal lazy onClose={onClose} isOpen={isOpen}>
         <LoginForm />
     </Modal>
 );
