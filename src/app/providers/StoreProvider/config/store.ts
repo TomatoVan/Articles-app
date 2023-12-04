51d9caecf632543dbcc5fb948eb9f58edb47a724
Leon.kov <kovzel.leo@gmail.com>
2023-12-04 19:36:06 +0300

    fix: add ts strict mode and fix ts errors

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,7 +1,8 @@
-import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
+import { configureStore, Reducer, ReducersMapObject } from '@reduxjs/toolkit';
 import { $api } from 'shared/api/api';
 import { To } from '@remix-run/router';
 import { NavigateOptions } from 'react-router/dist/lib/context';
+import { CombinedState } from 'redux';
 import { counterReducer } from '../../../../entities/Counter';
 import { userReducer } from '../../../../entities/User';
 import { StateSchema } from './StateSchema';
@@ -21,8 +22,7 @@ export function createReduxStore(
     const reducerManager = createReducerManager(rootReducers);
 
     const store = configureStore({
-        // @ts-ignore
-        reducer: reducerManager.reduce as ReducersMapObject<StateSchema>,
+        reducer: reducerManager.reduce as Reducer<CombinedState<StateSchema>>,
         devTools: __IS_DEV__,
         preloadedState: initialState,
         middleware: (getDefaultMiddleware) => getDefaultMiddleware({
