757dbd9d74bdd0139141af49cfb9fa9e85298da4
Leon.kov <kovzel.leo@gmail.com>
2024-02-14 14:36:01 +0300

    fix: themes colors

--- src/pages/ArticleEditPage/ui/ArticleEditPage.tsx
+++ src/pages/ArticleEditPage/ui/ArticleEditPage.tsx
@@ -1,20 +1,17 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import { Page } from 'widgets/Page/Page';
 import { useParams } from 'react-router-dom';
-import cls from './ArticleEditPage.module.scss';
 
 interface ArticleEditPageProps {
-	className?: string;
+  className?: string;
 }
 const ArticleEditPage = memo((props: ArticleEditPageProps) => {
     const { className } = props;
-    const { t } = useTranslation();
     const { id } = useParams<{ id: string }>();
     const isEdit = Boolean(id);
     return (
-        <Page className={classNames(cls.ArticleEditPage, {}, [className])}>
+        <Page className={classNames('', {}, [className])}>
             {isEdit ? 'Edit article' : 'Create new article'}
         </Page>
     );
