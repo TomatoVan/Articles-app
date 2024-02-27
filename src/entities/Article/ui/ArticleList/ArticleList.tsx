d6b6cd2389080b1fd6b18ab5ec1abea03ff50dcc
Leon.kov <kovzel.leo@gmail.com>
2024-02-27 21:43:44 +0300

    feat: add circular dependency fixer webpack plugin

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -4,7 +4,7 @@ import { HTMLAttributeAnchorTarget, LegacyRef, memo } from 'react';
 import { Text, TextSize } from 'shared/ui/Text/Text';
 import { List, ListRowProps, WindowScroller } from 'react-virtualized';
 import { PAGE_ID } from 'widgets/Page/Page';
-import { ArticleView } from '../../../../entities/Article';
+import { ArticleView } from '../../model/consts/consts';
 import { ArticleListItemSkeleton } from '../ArticleListItem/ArticleListItemSkeleton';
 import { ArticleListItem } from '../ArticleListItem/ArticleListItem';
 import cls from './ArticleList.module.scss';
