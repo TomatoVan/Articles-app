f55772db9d8f23722a1a8781e9f5c578431044e9
Leon.kov <kovzel.leo@gmail.com>
2024-04-09 13:23:21 +0300

    feat: add initAuthData request, fix themes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,27 @@
 import React, { Suspense, useEffect } from 'react';
 import './styles/index.scss';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
 import { Navbar } from '@/widgets/Navbar';
 import { Sidebar } from '@/widgets/Sidebar';
-import { getUserInited, userActions } from '@/entities/User';
+import { getUserInited, initAuthData } from '@/entities/User';
 import { useTheme } from '@/shared/lib/hook/useTheme/useTheme';
+import { useAppDispatch } from '@/shared/lib/hook/useAppDispatch/useAppDispatch';
+import { PageLoader } from '@/features/PageLoader';
 
 function App() {
     const { theme } = useTheme();
-    const dispatch = useDispatch();
+    const dispatch = useAppDispatch();
     const inited = useSelector(getUserInited);
 
     useEffect(() => {
-        dispatch(userActions.initAuthData());
+        dispatch(initAuthData());
     }, [dispatch]);
+
+    if (!inited) {
+        return <PageLoader />;
+    }
 
     return (
         <div className={classNames('app', {}, [theme])}>
--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,5 +1,4 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { USER_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
 import { ThunkConfig } from '@/app/providers/StoreProvider';
 import { User, userActions } from '../../../../../entities/User';
 
@@ -20,10 +19,6 @@ export const loginByUsername = createAsyncThunk<
         if (!response.data) {
             throw new Error();
         }
-        localStorage.setItem(
-            USER_LOCALSTORAGE_KEY,
-            JSON.stringify(response.data),
-        );
 
         dispatch(userActions.setAuthData(response.data));
 
