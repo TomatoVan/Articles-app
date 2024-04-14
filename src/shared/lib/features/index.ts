ca3403f763967e040b8c0e6ff4cc7e366f3f5333
Leon.kov <kovzel.leo@gmail.com>
2024-04-14 13:49:31 +0300

    fix: toggle feature imports

--- src/shared/lib/features/index.ts
+++ src/shared/lib/features/index.ts
@@ -1,3 +1,3 @@
 export { setFeatureFlags, getFeatureFlag } from './setGetFeatures';
 export { toggleFeatures } from './toggleFeatures';
-export { ToggleFeatures } from './toggleFeatures/ToggleFeatures';
+export { ToggleFeatures } from './ToggleFeatures/ToggleFeatures';
