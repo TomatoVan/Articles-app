3f63cf30580786ab4e5b723bb5b11a7dc867f06e
Leon.kov <kovzel.leo@gmail.com>
2024-04-18 19:31:37 +0300

    feat: change sidebar, icons, buttons, links

---
+++ src/shared/ui/redesigned/Button/Button.tsx
@@ -0,0 +1,49 @@
+import { ButtonHTMLAttributes, memo, ReactNode } from 'react';
+import { classNames, Mods } from '@/shared/lib/classNames/classNames';
+import cls from './Button.module.scss';
+
+export type ButtonVariant = 'clear' | 'outline';
+
+export type ButtonSize = 'm' | 'l' | 'xl';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    className?: string;
+    variant?: ButtonVariant;
+    square?: boolean;
+    size?: ButtonSize;
+    isDisabled?: boolean;
+    children?: ReactNode;
+    fullWidth?: boolean;
+}
+
+export const Button = memo((props: ButtonProps) => {
+    const {
+        className,
+        children,
+        variant = 'outline',
+        square,
+        isDisabled,
+        size = 'm',
+        fullWidth,
+        ...otherProps
+    } = props;
+
+    const mods: Mods = {
+        [cls.square]: square,
+        [cls.isDisabled]: isDisabled,
+        [cls.fullWidth]: fullWidth,
+    };
+
+    const additional = [className, cls[variant], cls[size]];
+
+    return (
+        <button
+            type="button"
+            className={classNames(cls.Button, mods, additional)}
+            disabled={isDisabled}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    );
+});
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,7 +1,6 @@
 import { memo, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Button, ButtonSize, ButtonTheme } from '@/shared/ui/deprecated/Button';
-import { AppLogo } from '@/shared/ui/deprecated/AppLogo';
 import { VStack } from '@/shared/ui/deprecated/Stack';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { getSidebarItems } from '../../model/selectors/getSidebarItems';
@@ -10,6 +9,9 @@ import cls from './Sidebar.module.scss';
 import { LangSwitcher } from '../../../../features/LangSwitcher';
 import { ThemeSwitcher } from '@/features/ThemeSwitcher';
 import { ToggleFeatures } from '@/shared/lib/features';
+import { AppLogo } from '@/shared/ui/redesigned/AppLogo';
+import { Icon } from '@/shared/ui/redesigned/Icon';
+import ArrowIcon from '@/shared/assets/icons/arrow-bottom.svg';
 
 interface SidebarProps {
     className?: string;
@@ -44,11 +46,28 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
                     data-testid="sidebar"
                     className={classNames(
                         cls.SidebarRedesigned,
-                        { [cls.collapsed]: collapsed },
+                        { [cls.collapsedRedesigned]: collapsed },
                         [className],
                     )}
                 >
-                    <AppLogo className={cls.appLogo} />
+                    <AppLogo
+                        size={collapsed ? 30 : 50}
+                        className={cls.appLogo}
+                    />
+                    <VStack role="navigation" gap="8" className={cls.items}>
+                        {itemsList}
+                    </VStack>
+                    <Icon
+                        Svg={ArrowIcon}
+                        data-testid="sidebar-toggle"
+                        onClick={onToggle}
+                        className={cls.collapseBtn}
+                        clickable
+                    />
+                    <div className={cls.switchers}>
+                        <ThemeSwitcher />
+                        <LangSwitcher short={collapsed} className={cls.lang} />
+                    </div>
                 </aside>
             }
             off={
